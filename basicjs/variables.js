74cbb1e0dee1b1d6f239da587f132711236812a8
Khawaja-Muzamil <162344545+Khawaja-Muzamil@users.noreply.github.com>
2024-03-19 01:54:50 +0000

    added new file

---
+++ basicjs/variables.js
@@ -0,0 +1,12 @@
+const accountId=144553;
+let accountEmail="gchbxjabhc";
+var accountPassword=354678;
+accountCity="kashmir";
+
+// accountId=2;
+console.log(accountId);
+accountEmail="dfghjklkjh"
+accountPassword=12345
+accountCity='banglor'
+
+console.table([accountEmail,accountId,accountPassword,accountCity])
