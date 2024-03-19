70f1681ca47db123482a70143883273ae602b042
Khawaja-Muzamil <162344545+Khawaja-Muzamil@users.noreply.github.com>
2024-03-19 02:42:10 +0000

    add new file

---
+++ basicjs/datatypes.js
@@ -0,0 +1,19 @@
+"use strict"
+
+console.log(3+3);
+// types of datatypes
+// number= 2 to power 53
+// string=""
+// boolean= true / false
+//  null = standalone value(representation of empty value)
+// undefined (value which is not defined)
+// symbol = uniqueness.
+
+// objects
+
+console.log(typeof 234);
+console.log(typeof null);
+console.log(typeof 'age');
+console.log(typeof undefined);
+
+
